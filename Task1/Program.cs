bc0a349c3028d832a8ac3cabfc3a21ef248c4099
dell <dell@ENGALI>
2025-01-19 13:41:55 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Tax = 0.06;
+            Console.WriteLine("Islam's Carpet Cleaning Service");
+            Console.WriteLine("How many small Carpet do you want to clean? ");
+            int nums = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many large Carpet do you want to clean? ");
+            int numl = Convert.ToInt32(Console.ReadLine());
+
+            double cost = (nums * 25) + (numl * 35);
+            Console.WriteLine($"Cost: {cost}");
+            
+            double taxvalue = cost * Tax;
+            Console.WriteLine($"Tax: {taxvalue}");
+
+            double total = cost + taxvalue;
+            Console.WriteLine($"Total estimate: {total} , This estimate is valid for 30 days");  
+        }
+    }
+}
---
+++ data types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Transactions;
+
+namespace data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine(); //read
+            Console.WriteLine($"your name is: {name}"); //write
+
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"your age is: {age}");*/
+
+            Console.WriteLine("Enter your favorite number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"no really!!, {num} is favorite number");
+
+        }
+    }
+}
